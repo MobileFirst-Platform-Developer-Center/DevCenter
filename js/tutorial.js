ecdc76a67331e75cbaa04057583ff18711c5b530
Nathan Hazout <nathanh@il.ibm.com>
2015-10-22 17:29:18 +0300

    Prevent error when no opened category

--- js/tutorial.js
+++ js/tutorial.js
@@ -1,5 +1,7 @@
-var openedItem = $('.tutorial .col-sm-2 .navmenu-default li.open')[0];
-openedItem.closable = false;
+var openedItems = $('.tutorial .col-sm-2 .navmenu-default li.open');
+if(openedItems.length){
+  openedItems[0].closable = false;
+}
 
 $('.tutorial .col-sm-2 .navmenu-default li').on({
     "shown.bs.dropdown": function() {
