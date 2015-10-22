8865987b8f7ee5f3c9425e9ded2e86b96b819e44
Nathan Hazout <nathanh@il.ibm.com>
2015-10-22 17:05:06 +0300

    Playing with closable menus

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
-$('.dropdown-menu').on('hide.bs.dropdown', function () {
-    return false;
+var openedItem = $('.tutorial .col-sm-2 .navmenu-default li.open')[0];
+openedItem.closable = false;
+
+$('.tutorial .col-sm-2 .navmenu-default li').on({
+    "shown.bs.dropdown": function() {
+      this.closable = false;
+    },
+    "click":             function() {
+      this.closable = true;
+    },
+    "hide.bs.dropdown":  function() {
+      return this.closable;
+    }
 });
