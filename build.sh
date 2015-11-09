dd55479fabd25f45dae121fbabe06649758173bc
Nathan Hazout <nathanh@il.ibm.com>
2015-11-09 13:22:32 +0200

    #72 Trying verbose version again

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ fi
 set -e
 
 # build site with jekyll, by default to `_site' folder
-bundle exec jekyll build -V
+bundle exec jekyll build --verbose
 
 # cleanup
 rm -rf ../mfpsamples.github.ibm.com.master
