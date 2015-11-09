5b21ce0c4babc56ef393e18227bcaa6346b25331
Nathan Hazout <nathanh@il.ibm.com>
2015-11-09 12:07:45 +0200

    #72 Swaping branches

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 This is the working area for the IBM MobileFirst Platform Developer Center v3.0.
-
-test
--- build.sh
+++ build.sh
@@ -13,19 +13,19 @@ set -e
 bundle exec jekyll build
 
 # cleanup
-rm -rf ../mfpsamples.github.ibm.com.generated
+rm -rf ../mfpsamples.github.ibm.com.master
 
 #clone `master' branch of the repository using encrypted GH_TOKEN for authentification
-git clone git@github.ibm.com:MFPSamples/mfpsamples.github.ibm.com.git --branch generated --single-branch ../mfpsamples.github.ibm.com.generated
+git clone git@github.ibm.com:MFPSamples/mfpsamples.github.ibm.com.git --branch master --single-branch ../mfpsamples.github.ibm.com.master
 # copy generated HTML site to `master' branch
-rm -rf ../mfpsamples.github.ibm.com.generated/*
-cp -R _site/* ../mfpsamples.github.ibm.com.generated
+rm -rf ../mfpsamples.github.ibm.com.master/*
+cp -R _site/* ../mfpsamples.github.ibm.com.master
 
 # commit and push generated content to `master' branch
 # since repository was cloned in write mode with token auth - we can push there
-cd ../mfpsamples.github.ibm.com.generated
+cd ../mfpsamples.github.ibm.com.master
 git config user.email "nathanh@il.ibm.com"
 git config user.name "Nathan Hazout Travis"
 git add -A .
 git commit -a -m "Travis Build $TRAVIS_BUILD_NUMBER"
-git push --quiet origin generated
+git push --quiet origin master
