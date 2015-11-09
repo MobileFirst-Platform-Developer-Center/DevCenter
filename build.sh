c21cdc339f3cf0e264df31b1ac3e4f59434f0085
Nathan Hazout <nathanh@il.ibm.com>
2015-11-09 08:27:46 +0200

    #72 experimenting with travis

---
+++ build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# only proceed script when started not by pull request (PR)
+if [ $TRAVIS_PULL_REQUEST == "true" ]; then
+  echo "this is PR, exiting"
+  exit 0
+fi
+
+# enable error reporting to the console
+set -e
+
+# build site with jekyll, by default to `_site' folder
+jekyll build
+
+# cleanup
+rm -rf ../mfpsamples.github.ibm.com.generated
+
+#clone `master' branch of the repository using encrypted GH_TOKEN for authentification
+git clone <url> --branch <branch> --single-branch
+git clone git@github.ibm.com:MFPSamples/mfpsamples.github.ibm.com.git --branch generated --single-branch ../mfpsamples.github.ibm.com.generated
+# copy generated HTML site to `master' branch
+cp -R _site/* ../mfpsamples.github.ibm.com.generated
+
+# commit and push generated content to `master' branch
+# since repository was cloned in write mode with token auth - we can push there
+cd ../mfpsamples.github.ibm.com.generated
+git add -A .
+git commit -a -m "Travis #$TRAVIS_BUILD_NUMBER"
+git push --quiet origin master > /dev/null 2>&1
