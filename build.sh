6c545c135732daa0792d8f2326b9264942e43dad
Nathan Hazout <nathanh@il.ibm.com>
2015-11-09 11:52:23 +0200

    #72

--- build.sh
+++ build.sh
@@ -18,6 +18,7 @@ rm -rf ../mfpsamples.github.ibm.com.generated
 #clone `master' branch of the repository using encrypted GH_TOKEN for authentification
 git clone git@github.ibm.com:MFPSamples/mfpsamples.github.ibm.com.git --branch generated --single-branch ../mfpsamples.github.ibm.com.generated
 # copy generated HTML site to `master' branch
+rm -rf ../mfpsamples.github.ibm.com.generated/*
 cp -R _site/* ../mfpsamples.github.ibm.com.generated
 
 # commit and push generated content to `master' branch
@@ -26,5 +27,5 @@ cd ../mfpsamples.github.ibm.com.generated
 git config user.email "nathanh@il.ibm.com"
 git config user.name "Nathan Hazout Travis"
 git add -A .
-git commit -a -m "Travis #$TRAVIS_BUILD_NUMBER"
+git commit -a -m "Travis Build $TRAVIS_BUILD_NUMBER"
 git push --quiet origin generated
