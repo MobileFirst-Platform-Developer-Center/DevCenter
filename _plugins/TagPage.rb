168f336d28763aed48d73751993c25160b303c1b
Nathan Hazout <nathanh@il.ibm.com>
2015-11-10 16:45:54 +0200

    #62 auto-generate atom by tag

--- _plugins/TagPage.rb
+++ _plugins/TagPage.rb
@@ -1,6 +1,4 @@
 module Jekyll
-  require 'pry'
-  require 'pry-byebug'
 
   class TagPage < Page
     def initialize(site, base, dir, tag)
@@ -20,15 +18,32 @@ module Jekyll
     end
   end
 
+  class TagAtom < Page
+    def initialize(site, base, dir, tag)
+      @site = site
+      @base = base
+      @dir = dir
+      @name = "#{tag}.xml"
+
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), 'atom.html')
+      self.data['tag'] = tag
+
+      # category_title_prefix = site.config['category_title_prefix'] || 'Category: '
+      # self.data['title'] = "#{category_title_prefix}#{category}"
+      self.data['title'] = "#{tag}"
+      self.data['filterByTag'] = "#{tag}"
+    end
+  end
+
   class TagPageGenerator < Generator
     safe true
 
     def generate(site)
       if site.layouts.key? 'tagpage'
-        # binding.pry
-        dir = site.config['tag_dir'] || 'blog/tag'
         site.tags.each_key do |tag|
-          site.pages << TagPage.new(site, site.source, File.join(dir, tag), tag)
+          site.pages << TagPage.new(site, site.source, File.join('blog/tag', tag), tag)
+          site.pages << TagAtom.new(site, site.source, File.join('blog/atom'), tag)
         end
       end
     end
