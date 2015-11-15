252fc8988a8bb767aef620258b78f8f71b5d69db
Nathan Hazout <nathanh@il.ibm.com>
2015-11-15 16:24:18 +0200

    #71 same features new breadcrumb

--- _plugins/Breadcrumbs.rb
+++ _plugins/Breadcrumbs.rb
@@ -1,7 +1,7 @@
 #require 'pry'
 #require 'pry-byebug'
 
-def getPage(url,site)
+def getPageByUrl(url,site)
   site['pages'].each do |page|
 
     if page.url == url
@@ -9,6 +9,46 @@ def getPage(url,site)
     end
   end
   return nil
+end
+
+def GetSiblings(url,site)
+  parts = url.split('/')
+  target_level = parts.count
+  parts.pop()
+  parent_url = parts.join('/') + '/'
+
+  siblings = []
+
+  site['pages'].each do |page|
+    level = page.url.split('/').count
+    if (level == target_level) && (page.url.include? parent_url)
+      element = {}
+
+      if page.data['use_dropdown_home']
+        element['url'] = page.url + page.data['use_dropdown_home']
+      else
+        element['url'] = page.url
+      end
+
+      if(page.data['breadcrumb_title'])
+        element['title'] = page.data['breadcrumb_title']
+      else
+        element['title'] = page.data['title']
+      end
+
+      if page.data['weight']
+        element['weight'] = page.data['weight']
+      else
+        element['weight'] = 1000
+      end
+
+      siblings.push(element)
+    end
+  end
+
+  siblings = siblings.sort {|x,y| x['weight'] <=> y['weight'] }
+  # binding.pry
+  return siblings
 end
 
 Jekyll::Hooks.register :pages, :pre_render do |page, payload|
@@ -22,11 +62,23 @@ Jekyll::Hooks.register :pages, :pre_render do |page, payload|
       aggregated = aggregated + part + '/'
 
       if i > 1
-        temp = getPage(aggregated,payload['site'])
+        temp = getPageByUrl(aggregated,payload['site'])
         if temp
           element = {}
           element['url'] = temp.url
-          element['title'] = temp.data['title']
+
+          if(temp.data['breadcrumb_title'])
+            element['title'] = temp.data['breadcrumb_title']
+          else
+            element['title'] = temp.data['title']
+          end
+
+          if(temp.data['use_dropdown'])
+            element['use_dropdown'] = true
+            element['siblings'] = GetSiblings(temp.url,payload['site'])
+          else
+            element['use_dropdown'] = false
+          end
           breadcrumbs.push(element)
         end
       end
