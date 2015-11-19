23234f3acff5f0adb2582fa7f012b3212a5bb400
Nathan Hazout <nathanh@il.ibm.com>
2015-11-19 09:42:39 +0200

    #85 fix pdf trigger

--- _plugins/ToPDF.rb
+++ _plugins/ToPDF.rb
@@ -5,24 +5,25 @@ require 'fileutils'
 PDFKit.configure do |config|
   # config.wkhtmltopdf = '/path/to/wkhtmltopdf'
   config.default_options = {
-    :page_size => 'A4',
-    :print_media_type => true
+    page_size: 'A4',
+    print_media_type: true
   }
   # Use only if your external hostname is unavailable on the server.
   # config.root_url = "http://localhost"
   # config.verbose = false
 end
 
-Jekyll::Hooks.register :pages, :post_write do |page|
-  #binding.pry
-  if page.data["print_pdf"]
-    prefix_folder = '_site/pdf'
+Jekyll::Hooks.register :site, :post_write do |site|
+  site.pages.each do |page|
+    next unless page.data['print_pdf']
+    # binding.pry
+    prefix_folder = './_site/pdf'
     # binding.pry
     # Create the directory if not exists
     parts = page.url.split('/')
-    parts.pop()
+    parts.pop
     parent_folder = prefix_folder + parts.join('/') + '/'
-    FileUtils.mkdir_p(parent_folder) unless File.exists?(parent_folder)
+    FileUtils.mkdir_p(parent_folder) unless File.exist?(parent_folder)
 
     # Define the file path
     file_name = prefix_folder + page.url.chomp('/') + '.pdf'
