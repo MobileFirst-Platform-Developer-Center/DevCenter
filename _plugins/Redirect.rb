81b6a3983879bae5a8845a430d7e47c34f9b6465
Nathan Hazout <nathanh@il.ibm.com>
2015-11-29 14:08:41 +0200

    #40 redirect blog posts

--- _plugins/Redirect.rb
+++ _plugins/Redirect.rb
@@ -19,21 +19,29 @@ module Jekyll
   class RedirectPageGenerator < Generator
     safe true
 
-    # Look for pages that need redirection
     def generate(site)
       if site.layouts.key? 'redirect'
+        # Look for pages to redirect
         site.pages.each do |page|
           next unless (page['is_redirect'] != true)
           # binding.pry
           old_url = page.url.sub '/tutorials/en/foundation/7.0/', 'documentation/getting-started-7-0/'
           old_url = old_url.sub '/tutorials/en/foundation/6.3/', 'documentation/getting-started-6-3/'
           old_url = old_url.sub '/tutorials/en/foundation/7.1/', 'documentation/getting-started-7-1/foundation/'
-          
+
           old_url = old_url.sub '/tutorials/en/product-integration/7.1/', 'documentation/integration-7-1/'
           old_url = old_url.sub '/tutorials/en/product-integration/7.0/', 'documentation/integration-7-0/'
           old_url = old_url.sub '/tutorials/en/product-integration/6.3/', 'documentation/integration-6-3/'
           next unless old_url != page.url
           new_url = site.baseurl + page.url
+          site.pages << RedirectPage.new(site, site.source, File.join(old_url), new_url)
+        end
+
+        # Look for blogs to redirect
+        site.posts.each do |post|
+          # Currently making a redirect for every post, but need to limit to those published before DC 3 was released.
+          old_url = post.date.strftime('%Y/%m/%d') + '/' + post.data['slug'] + '/'
+          new_url = site.baseurl + post.url
           site.pages << RedirectPage.new(site, site.source, File.join(old_url), new_url)
         end
       end
