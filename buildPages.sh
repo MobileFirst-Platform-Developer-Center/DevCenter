af0e2c830984abe3fe943748ef5a1a4e551cae3a
Nathan Hazout <nathanh@il.ibm.com>
2015-12-01 14:44:53 +0200

    Experimenting with Travis and Bluemix

--- buildPages.sh
+++ buildPages.sh
@@ -33,30 +33,3 @@ git config user.name "Nathan Hazout Travis"
 git add -A .
 git commit -a -m "Travis Build $TRAVIS_BUILD_NUMBER"
 git push --quiet origin master
-
-pwd
-
-# ## Second, build for Bluemix
-# cd ~
-# # build site with jekyll, by default to `_site' folder
-# bundle exec jekyll build --config _config.yml,_configBluemix.yml -d _site --profile
-# rm -f _site/*.log
-# # bundle exec htmlproof ./_site --disable-external --href-ignore '#'
-#
-# # cleanup
-# rm -rf ../mfpsamples.github.ibm.com.generated-bluemix
-#
-# #clone `generated-bluemix' branch of the repository
-# git clone git@github.ibm.com:MFPSamples/mfpsamples.github.ibm.com.git --branch generated-bluemix --single-branch ../mfpsamples.github.ibm.com.generated-bluemix
-# # copy generated HTML site to `generated-bluemix' branch
-# rm -rf ../mfpsamples.github.ibm.com.generated-bluemix/*
-# cp -R _site/* ../mfpsamples.github.ibm.com.generated-bluemix
-#
-# # commit and push generated content to `master' branch
-# # since repository was cloned in write mode with token auth - we can push there
-# cd ../mfpsamples.github.ibm.com.generated-bluemix
-# git config user.email "nathanh@il.ibm.com"
-# git config user.name "Nathan Hazout Travis"
-# git add -A .
-# git commit -a -m "Travis Build $TRAVIS_BUILD_NUMBER"
-# git push --quiet origin master
