25938f5bb03cbf8f089beefc67287746e4895e65
Nathan Hazout <nathanh@il.ibm.com>
2015-12-03 13:58:37 +0200

    #85 try generating separate branches for pdf

--- buildPDF.sh
+++ buildPDF.sh
@@ -9,27 +9,38 @@ fi
 # enable error reporting to the console
 set -e
 
-## First, build for GitHub Pages
 # build site with jekyll, by default to `_site' folder
 rm -rf _site/*
 bundle exec jekyll build --config _config.yml,_configPDF.yml -d _site --profile
 rm -f _site/*.log
 # bundle exec htmlproof ./_site --disable-external --href-ignore '#'
 
-# cleanup
-rm -rf ../generated-pdf
-
+## Push the `master`
 #clone pdf repository
-git clone git@github.ibm.com:NATHANH/experimentalpdf.git --branch master --single-branch ../generated-pdf
-# copy generated PDF site to `generated-pdf' branch
-rm -rf ../generated-pdf/*
-cp -R _site/pdf/* ../generated-pdf
-
-# commit and push generated content to `generated-bluemix' branch
-# since repository was cloned in write mode with token auth - we can push there
-cd ../generated-pdf
+git clone git@github.ibm.com:NATHANH/experimentalpdf.git --branch master --single-branch ../generated-pdf/master
+# copy generated PDF
+rm -rf ../generated-pdf/master/*
+cp -R _site/pdf/* ../generated-pdf/master
+# commit and push generated content
+cd ../generated-pdf/master
 git config user.email "nathanh@il.ibm.com"
 git config user.name "Nathan Hazout Travis"
 git add -A .
 git commit -a -m "Travis Build $TRAVIS_BUILD_NUMBER"
 git push --quiet origin master
+
+## Push the `8.0`
+#clone pdf repository
+git clone git@github.ibm.com:NATHANH/experimentalpdf.git --branch master --single-branch ../generated-pdf/release80
+# copy generated PDF
+rm -rf ../generated-pdf/release80/*
+cp -R --parents _site/pdf/tutorials/en/foundation/8.0/* ../generated-pdf/release80
+cp _site/pdf/8.0.html ../generated-pdf/release80/
+
+# commit and push generated content
+cd ../generated-pdf/release80
+git config user.email "nathanh@il.ibm.com"
+git config user.name "Nathan Hazout Travis"
+git add -A .
+git commit -a -m "Travis Build $TRAVIS_BUILD_NUMBER"
+git push --quiet origin release80
