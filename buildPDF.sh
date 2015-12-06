00499cddebdc56e733bec2117e15768ff63f7497
Nathan Hazout <nathanh@il.ibm.com>
2015-12-06 13:25:58 +0200

    #85 application scanning

--- buildPDF.sh
+++ buildPDF.sh
@@ -42,8 +42,8 @@ rm -rf ../generated-pdf/release80/*
 mkdir -p ../generated-pdf/release80/tutorials/en/foundation/8.0 && cp -R _site/pdf/tutorials/en/foundation/8.0/* ../generated-pdf/release80/tutorials/en/foundation/8.0/
 mkdir -p ../generated-pdf/release80/tutorials/en/product-integration/8.0 && cp -R _site/pdf/tutorials/en/product-integration/8.0/* ../generated-pdf/release80/tutorials/en/product-integration/8.0/
 mkdir -p ../generated-pdf/release80/tutorials/en/quality-assurance/8.0 && cp -R _site/pdf/tutorials/en/quality-assurance/8.0/* ../generated-pdf/release80/tutorials/en/quality-assurance/8.0/
-mkdir -p ../generated-pdf/release80/tutorials/en/application-scanning && cp -R _site/pdf/tutorials/en/application-scanning/* ../generated-pdf/release80/tutorials/en/application-scanning/
 mkdir -p ../generated-pdf/release80/tutorials/en/cloudant && cp -R _site/pdf/tutorials/en/cloudant/* ../generated-pdf/release80/tutorials/en/cloudant/
+cp -R _site/pdf/tutorials/en/application-scanning.pdf ../generated-pdf/release80/tutorials/en/application-scanning.pdf
 # copy the index file
 cp _site/pdf/8.0.html ../generated-pdf/release80/index.html
 # commit and push generated content
@@ -65,8 +65,8 @@ rm -rf ../generated-pdf/release71/*
 mkdir -p ../generated-pdf/release71/tutorials/en/foundation/7.1 && cp -R _site/pdf/tutorials/en/foundation/7.1/* ../generated-pdf/release71/tutorials/en/foundation/7.1/
 mkdir -p ../generated-pdf/release71/tutorials/en/product-integration/7.1 && cp -R _site/pdf/tutorials/en/product-integration/7.1/* ../generated-pdf/release71/tutorials/en/product-integration/7.1/
 mkdir -p ../generated-pdf/release71/tutorials/en/quality-assurance/7.1 && cp -R _site/pdf/tutorials/en/quality-assurance/7.1/* ../generated-pdf/release71/tutorials/en/quality-assurance/7.1/
-mkdir -p ../generated-pdf/release71/tutorials/en/application-scanning && cp -R _site/pdf/tutorials/en/application-scanning/* ../generated-pdf/release71/tutorials/en/application-scanning/
 mkdir -p ../generated-pdf/release71/tutorials/en/cloudant && cp -R _site/pdf/tutorials/en/cloudant/* ../generated-pdf/release71/tutorials/en/cloudant/
+cp -R _site/pdf/tutorials/en/application-scanning.pdf ../generated-pdf/release71/tutorials/en/application-scanning.pdf
 # copy the index file
 cp _site/pdf/7.1.html ../generated-pdf/release71/index.html
 # commit and push generated content
@@ -87,8 +87,8 @@ git clone git@github.ibm.com:NATHANH/experimentalpdf.git --branch release70 --si
 rm -rf ../generated-pdf/release70/*
 mkdir -p ../generated-pdf/release70/tutorials/en/foundation/7.0 && cp -R _site/pdf/tutorials/en/foundation/7.0/* ../generated-pdf/release70/tutorials/en/foundation/7.0/
 mkdir -p ../generated-pdf/release70/tutorials/en/product-integration/7.0 && cp -R _site/pdf/tutorials/en/product-integration/7.0/* ../generated-pdf/release70/tutorials/en/product-integration/7.0/
-mkdir -p ../generated-pdf/release70/tutorials/en/application-scanning && cp -R _site/pdf/tutorials/en/application-scanning/* ../generated-pdf/release70/tutorials/en/application-scanning/
 mkdir -p ../generated-pdf/release70/tutorials/en/cloudant && cp -R _site/pdf/tutorials/en/cloudant/* ../generated-pdf/release70/tutorials/en/cloudant/
+cp -R _site/pdf/tutorials/en/application-scanning.pdf ../generated-pdf/release70/tutorials/en/application-scanning.pdf
 # copy the index file
 cp _site/pdf/7.0.html ../generated-pdf/release70/index.html
 # commit and push generated content
@@ -109,8 +109,8 @@ git clone git@github.ibm.com:NATHANH/experimentalpdf.git --branch release63 --si
 rm -rf ../generated-pdf/release63/*
 mkdir -p ../generated-pdf/release63/tutorials/en/foundation/6.3 && cp -R _site/pdf/tutorials/en/foundation/6.3/* ../generated-pdf/release63/tutorials/en/foundation/6.3/
 mkdir -p ../generated-pdf/release63/tutorials/en/product-integration/6.3 && cp -R _site/pdf/tutorials/en/product-integration/6.3/* ../generated-pdf/release63/tutorials/en/product-integration/6.3/
-mkdir -p ../generated-pdf/release63/tutorials/en/application-scanning && cp -R _site/pdf/tutorials/en/application-scanning/* ../generated-pdf/release63/tutorials/en/application-scanning/
 mkdir -p ../generated-pdf/release63/tutorials/en/cloudant && cp -R _site/pdf/tutorials/en/cloudant/* ../generated-pdf/release63/tutorials/en/cloudant/
+cp -R _site/pdf/tutorials/en/application-scanning.pdf ../generated-pdf/release63/tutorials/en/application-scanning.pdf
 # copy the index file
 cp _site/pdf/6.3.html ../generated-pdf/release63/index.html
 # commit and push generated content
