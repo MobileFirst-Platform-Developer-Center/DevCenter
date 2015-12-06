2331a337ed9c0611a6e552a3ca85c29111a53030
Nathan Hazout <nathanh@il.ibm.com>
2015-12-06 13:20:25 +0200

    #85 generate all 4 versions

--- buildPDF.sh
+++ buildPDF.sh
@@ -46,7 +46,6 @@ mkdir -p ../generated-pdf/release80/tutorials/en/application-scanning && cp -R _
 mkdir -p ../generated-pdf/release80/tutorials/en/cloudant && cp -R _site/pdf/tutorials/en/cloudant/* ../generated-pdf/release80/tutorials/en/cloudant/
 # copy the index file
 cp _site/pdf/8.0.html ../generated-pdf/release80/index.html
-
 # commit and push generated content
 cd ../generated-pdf/release80
 git config user.email "nathanh@il.ibm.com"
@@ -54,3 +53,70 @@ git config user.name "Nathan Hazout Travis"
 git add -A .
 git commit -a -m "Travis Build $TRAVIS_BUILD_NUMBER"
 git push --quiet origin release80
+
+
+## Push the `7.1`
+cd ../../DevCenter
+pwd
+#clone pdf repository
+git clone git@github.ibm.com:NATHANH/experimentalpdf.git --branch release71 --single-branch ../generated-pdf/release71
+# copy generated PDF
+rm -rf ../generated-pdf/release71/*
+mkdir -p ../generated-pdf/release71/tutorials/en/foundation/7.1 && cp -R _site/pdf/tutorials/en/foundation/7.1/* ../generated-pdf/release71/tutorials/en/foundation/7.1/
+mkdir -p ../generated-pdf/release71/tutorials/en/product-integration/7.1 && cp -R _site/pdf/tutorials/en/product-integration/7.1/* ../generated-pdf/release71/tutorials/en/product-integration/7.1/
+mkdir -p ../generated-pdf/release71/tutorials/en/quality-assurance/7.1 && cp -R _site/pdf/tutorials/en/quality-assurance/7.1/* ../generated-pdf/release71/tutorials/en/quality-assurance/7.1/
+mkdir -p ../generated-pdf/release71/tutorials/en/application-scanning && cp -R _site/pdf/tutorials/en/application-scanning/* ../generated-pdf/release71/tutorials/en/application-scanning/
+mkdir -p ../generated-pdf/release71/tutorials/en/cloudant && cp -R _site/pdf/tutorials/en/cloudant/* ../generated-pdf/release71/tutorials/en/cloudant/
+# copy the index file
+cp _site/pdf/7.1.html ../generated-pdf/release71/index.html
+# commit and push generated content
+cd ../generated-pdf/release71
+git config user.email "nathanh@il.ibm.com"
+git config user.name "Nathan Hazout Travis"
+git add -A .
+git commit -a -m "Travis Build $TRAVIS_BUILD_NUMBER"
+git push --quiet origin release71
+
+
+## Push the `7.0`
+cd ../../DevCenter
+pwd
+#clone pdf repository
+git clone git@github.ibm.com:NATHANH/experimentalpdf.git --branch release70 --single-branch ../generated-pdf/release70
+# copy generated PDF
+rm -rf ../generated-pdf/release70/*
+mkdir -p ../generated-pdf/release70/tutorials/en/foundation/7.0 && cp -R _site/pdf/tutorials/en/foundation/7.0/* ../generated-pdf/release70/tutorials/en/foundation/7.0/
+mkdir -p ../generated-pdf/release70/tutorials/en/product-integration/7.0 && cp -R _site/pdf/tutorials/en/product-integration/7.0/* ../generated-pdf/release70/tutorials/en/product-integration/7.0/
+mkdir -p ../generated-pdf/release70/tutorials/en/application-scanning && cp -R _site/pdf/tutorials/en/application-scanning/* ../generated-pdf/release70/tutorials/en/application-scanning/
+mkdir -p ../generated-pdf/release70/tutorials/en/cloudant && cp -R _site/pdf/tutorials/en/cloudant/* ../generated-pdf/release70/tutorials/en/cloudant/
+# copy the index file
+cp _site/pdf/7.0.html ../generated-pdf/release70/index.html
+# commit and push generated content
+cd ../generated-pdf/release70
+git config user.email "nathanh@il.ibm.com"
+git config user.name "Nathan Hazout Travis"
+git add -A .
+git commit -a -m "Travis Build $TRAVIS_BUILD_NUMBER"
+git push --quiet origin release70
+
+
+## Push the `6.3`
+cd ../../DevCenter
+pwd
+#clone pdf repository
+git clone git@github.ibm.com:NATHANH/experimentalpdf.git --branch release63 --single-branch ../generated-pdf/release63
+# copy generated PDF
+rm -rf ../generated-pdf/release63/*
+mkdir -p ../generated-pdf/release63/tutorials/en/foundation/6.3 && cp -R _site/pdf/tutorials/en/foundation/6.3/* ../generated-pdf/release63/tutorials/en/foundation/6.3/
+mkdir -p ../generated-pdf/release63/tutorials/en/product-integration/6.3 && cp -R _site/pdf/tutorials/en/product-integration/6.3/* ../generated-pdf/release63/tutorials/en/product-integration/6.3/
+mkdir -p ../generated-pdf/release63/tutorials/en/application-scanning && cp -R _site/pdf/tutorials/en/application-scanning/* ../generated-pdf/release63/tutorials/en/application-scanning/
+mkdir -p ../generated-pdf/release63/tutorials/en/cloudant && cp -R _site/pdf/tutorials/en/cloudant/* ../generated-pdf/release63/tutorials/en/cloudant/
+# copy the index file
+cp _site/pdf/6.3.html ../generated-pdf/release63/index.html
+# commit and push generated content
+cd ../generated-pdf/release63
+git config user.email "nathanh@il.ibm.com"
+git config user.name "Nathan Hazout Travis"
+git add -A .
+git commit -a -m "Travis Build $TRAVIS_BUILD_NUMBER"
+git push --quiet origin release63
