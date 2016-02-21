3602cc7e2f9ab2e398744239a60db49fbaf2ead0
Idan Adar <idan@adar.me>
2016-02-21 11:48:12 +0200

    Make accordion panels refocus

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ $(function() {
 
     jQuery("abbr.timeago").timeago();
 
-    $("#accordion").on("shown.bs.collapse", function() {
+    $(".accordion").on("shown.bs.collapse", function() {
       var myEl = $(this).find('.collapse.in').prev('.panel-heading');
 
       $('html, body').animate({
