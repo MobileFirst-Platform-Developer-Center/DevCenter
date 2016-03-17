1586d10f50fcf7b852f86b32fb19b507f2bb72e3
Nathan Hazout <nathanh@il.ibm.com>
2016-03-17 09:45:49 +0200

    test pdf without links

--- build/buildPDF.sh
+++ build/buildPDF.sh
@@ -38,65 +38,65 @@ git commit -a -m "Travis Build $TRAVIS_BUILD_NUMBER"
 git push --quiet origin release80
 
 
-## Push the `7.1`
-cd ../../DevCenter
-pwd
-#clone pdf repository
-git clone git@github.ibm.com:MFPSamples/TutorialsForOfflineReading.git --depth 1 --branch release71 --single-branch ../generated-pdf/release71
-# copy generated PDF
-rm -rf ../generated-pdf/release71/*
-mkdir -p ../generated-pdf/release71/tutorials/en/foundation/7.1 && cp -R _site/pdf/tutorials/en/foundation/7.1/* ../generated-pdf/release71/tutorials/en/foundation/7.1/
-mkdir -p ../generated-pdf/release71/tutorials/en/product-integration/7.1 && cp -R _site/pdf/tutorials/en/product-integration/7.1/* ../generated-pdf/release71/tutorials/en/product-integration/7.1/
-mkdir -p ../generated-pdf/release71/tutorials/en/quality-assurance/7.1 && cp -R _site/pdf/tutorials/en/quality-assurance/7.1/* ../generated-pdf/release71/tutorials/en/quality-assurance/7.1/
-cp -R _site/pdf/tutorials/en/application-scanning.pdf ../generated-pdf/release71/tutorials/en/application-scanning.pdf
-# copy the index file
-cp _site/pdf/7.1.html ../generated-pdf/release71/index.html
-# commit and push generated content
-cd ../generated-pdf/release71
-git config user.email "nathanh@il.ibm.com"
-git config user.name "Nathan Hazout Travis"
-git add -A .
-git commit -a -m "Travis Build $TRAVIS_BUILD_NUMBER"
-git push --quiet origin release71
-
-
-## Push the `7.0`
-cd ../../DevCenter
-pwd
-#clone pdf repository
-git clone git@github.ibm.com:MFPSamples/TutorialsForOfflineReading.git --depth 1 --branch release70 --single-branch ../generated-pdf/release70
-# copy generated PDF
-rm -rf ../generated-pdf/release70/*
-mkdir -p ../generated-pdf/release70/tutorials/en/foundation/7.0 && cp -R _site/pdf/tutorials/en/foundation/7.0/* ../generated-pdf/release70/tutorials/en/foundation/7.0/
-mkdir -p ../generated-pdf/release70/tutorials/en/product-integration/7.0 && cp -R _site/pdf/tutorials/en/product-integration/7.0/* ../generated-pdf/release70/tutorials/en/product-integration/7.0/
-cp -R _site/pdf/tutorials/en/application-scanning.pdf ../generated-pdf/release70/tutorials/en/application-scanning.pdf
-# copy the index file
-cp _site/pdf/7.0.html ../generated-pdf/release70/index.html
-# commit and push generated content
-cd ../generated-pdf/release70
-git config user.email "nathanh@il.ibm.com"
-git config user.name "Nathan Hazout Travis"
-git add -A .
-git commit -a -m "Travis Build $TRAVIS_BUILD_NUMBER"
-git push --quiet origin release70
-
-
-## Push the `6.3`
-cd ../../DevCenter
-pwd
-#clone pdf repository
-git clone git@github.ibm.com:MFPSamples/TutorialsForOfflineReading.git --depth 1 --branch release63 --single-branch ../generated-pdf/release63
-# copy generated PDF
-rm -rf ../generated-pdf/release63/*
-mkdir -p ../generated-pdf/release63/tutorials/en/foundation/6.3 && cp -R _site/pdf/tutorials/en/foundation/6.3/* ../generated-pdf/release63/tutorials/en/foundation/6.3/
-mkdir -p ../generated-pdf/release63/tutorials/en/product-integration/6.3 && cp -R _site/pdf/tutorials/en/product-integration/6.3/* ../generated-pdf/release63/tutorials/en/product-integration/6.3/
-cp -R _site/pdf/tutorials/en/application-scanning.pdf ../generated-pdf/release63/tutorials/en/application-scanning.pdf
-# copy the index file
-cp _site/pdf/6.3.html ../generated-pdf/release63/index.html
-# commit and push generated content
-cd ../generated-pdf/release63
-git config user.email "nathanh@il.ibm.com"
-git config user.name "Nathan Hazout Travis"
-git add -A .
-git commit -a -m "Travis Build $TRAVIS_BUILD_NUMBER"
-git push --quiet origin release63
+# ## Push the `7.1`
+# cd ../../DevCenter
+# pwd
+# #clone pdf repository
+# git clone git@github.ibm.com:MFPSamples/TutorialsForOfflineReading.git --depth 1 --branch release71 --single-branch ../generated-pdf/release71
+# # copy generated PDF
+# rm -rf ../generated-pdf/release71/*
+# mkdir -p ../generated-pdf/release71/tutorials/en/foundation/7.1 && cp -R _site/pdf/tutorials/en/foundation/7.1/* ../generated-pdf/release71/tutorials/en/foundation/7.1/
+# mkdir -p ../generated-pdf/release71/tutorials/en/product-integration/7.1 && cp -R _site/pdf/tutorials/en/product-integration/7.1/* ../generated-pdf/release71/tutorials/en/product-integration/7.1/
+# mkdir -p ../generated-pdf/release71/tutorials/en/quality-assurance/7.1 && cp -R _site/pdf/tutorials/en/quality-assurance/7.1/* ../generated-pdf/release71/tutorials/en/quality-assurance/7.1/
+# cp -R _site/pdf/tutorials/en/application-scanning.pdf ../generated-pdf/release71/tutorials/en/application-scanning.pdf
+# # copy the index file
+# cp _site/pdf/7.1.html ../generated-pdf/release71/index.html
+# # commit and push generated content
+# cd ../generated-pdf/release71
+# git config user.email "nathanh@il.ibm.com"
+# git config user.name "Nathan Hazout Travis"
+# git add -A .
+# git commit -a -m "Travis Build $TRAVIS_BUILD_NUMBER"
+# git push --quiet origin release71
+#
+#
+# ## Push the `7.0`
+# cd ../../DevCenter
+# pwd
+# #clone pdf repository
+# git clone git@github.ibm.com:MFPSamples/TutorialsForOfflineReading.git --depth 1 --branch release70 --single-branch ../generated-pdf/release70
+# # copy generated PDF
+# rm -rf ../generated-pdf/release70/*
+# mkdir -p ../generated-pdf/release70/tutorials/en/foundation/7.0 && cp -R _site/pdf/tutorials/en/foundation/7.0/* ../generated-pdf/release70/tutorials/en/foundation/7.0/
+# mkdir -p ../generated-pdf/release70/tutorials/en/product-integration/7.0 && cp -R _site/pdf/tutorials/en/product-integration/7.0/* ../generated-pdf/release70/tutorials/en/product-integration/7.0/
+# cp -R _site/pdf/tutorials/en/application-scanning.pdf ../generated-pdf/release70/tutorials/en/application-scanning.pdf
+# # copy the index file
+# cp _site/pdf/7.0.html ../generated-pdf/release70/index.html
+# # commit and push generated content
+# cd ../generated-pdf/release70
+# git config user.email "nathanh@il.ibm.com"
+# git config user.name "Nathan Hazout Travis"
+# git add -A .
+# git commit -a -m "Travis Build $TRAVIS_BUILD_NUMBER"
+# git push --quiet origin release70
+#
+#
+# ## Push the `6.3`
+# cd ../../DevCenter
+# pwd
+# #clone pdf repository
+# git clone git@github.ibm.com:MFPSamples/TutorialsForOfflineReading.git --depth 1 --branch release63 --single-branch ../generated-pdf/release63
+# # copy generated PDF
+# rm -rf ../generated-pdf/release63/*
+# mkdir -p ../generated-pdf/release63/tutorials/en/foundation/6.3 && cp -R _site/pdf/tutorials/en/foundation/6.3/* ../generated-pdf/release63/tutorials/en/foundation/6.3/
+# mkdir -p ../generated-pdf/release63/tutorials/en/product-integration/6.3 && cp -R _site/pdf/tutorials/en/product-integration/6.3/* ../generated-pdf/release63/tutorials/en/product-integration/6.3/
+# cp -R _site/pdf/tutorials/en/application-scanning.pdf ../generated-pdf/release63/tutorials/en/application-scanning.pdf
+# # copy the index file
+# cp _site/pdf/6.3.html ../generated-pdf/release63/index.html
+# # commit and push generated content
+# cd ../generated-pdf/release63
+# git config user.email "nathanh@il.ibm.com"
+# git config user.name "Nathan Hazout Travis"
+# git add -A .
+# git commit -a -m "Travis Build $TRAVIS_BUILD_NUMBER"
+# git push --quiet origin release63
