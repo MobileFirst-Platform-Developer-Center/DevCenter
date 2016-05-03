2677ee9883a12191f745f5c3724e30d4ed47b69b
Idan Adar <idan@adar.me>
2016-05-03 08:53:57 +0300

    Disable "last modified on" -- not working... :/

--- _plugins/LastModifiedDate.rb
+++ _plugins/LastModifiedDate.rb
@@ -1,8 +1,9 @@
-Jekyll::Hooks.register :posts, :pre_render do |post|
+# Do not use. Does not work.
+#Jekyll::Hooks.register :posts, :pre_render do |post|
 
   # get the current post last modified time
-  modification_time = File.mtime( post.path )
+  # modification_time = File.mtime( post.path )
 
   # inject modification_time in post's datas.
-  post.data['last-modified-date'] = modification_time
-end
+  # post.data['last-modified-date'] = modification_time
+#3end
