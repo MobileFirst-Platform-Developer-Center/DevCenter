d24cce6218ecd75c89304cada7970b4bd6ce099e
Idan Adar <iadar@il.ibm.com>
2016-09-29 22:19:34 +0300

    Don’t look for appscan document
    Signed-off-by: Idan Adar <iadar@il.ibm.com>

--- build/buildPDF.sh
+++ build/buildPDF.sh
@@ -78,7 +78,7 @@ git clone git@github.ibm.com:MFPSamples/TutorialsForOfflineReading.git --depth 1
 rm -rf ../generated-pdf/release70/*
 mkdir -p ../generated-pdf/release70/tutorials/en/foundation/7.0 && cp -R _site/pdf/tutorials/en/foundation/7.0/* ../generated-pdf/release70/tutorials/en/foundation/7.0/
 mkdir -p ../generated-pdf/release70/tutorials/en/product-integration/7.0 && cp -R _site/pdf/tutorials/en/product-integration/7.0/* ../generated-pdf/release70/tutorials/en/product-integration/7.0/
-cp -R _site/pdf/tutorials/en/product-integration/7.0/application-scanning.pdf #../generated-pdf/release70/tutorials/en/application-scanning.pdf
+#cp -R _site/pdf/tutorials/en/product-integration/7.0/application-scanning.pdf ../generated-pdf/release70/tutorials/en/application-scanning.pdf
 # copy the index file
 cp _site/pdf/7.0.html ../generated-pdf/release70/index.html
 # commit and push generated content
