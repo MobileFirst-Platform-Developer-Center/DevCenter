1f3358325d27ebc3e2ab3f00beddf075210532b3
Idan Adar <iadar@il.ibm.com>
2016-09-29 22:05:09 +0300

    comment out…
    Signed-off-by: Idan Adar <iadar@il.ibm.com>

--- build/buildPDF.sh
+++ build/buildPDF.sh
@@ -57,7 +57,7 @@ rm -rf ../generated-pdf/release71/*
 mkdir -p ../generated-pdf/release71/tutorials/en/foundation/7.1 && cp -R _site/pdf/tutorials/en/foundation/7.1/* ../generated-pdf/release71/tutorials/en/foundation/7.1/
 mkdir -p ../generated-pdf/release71/tutorials/en/product-integration/7.1 && cp -R _site/pdf/tutorials/en/product-integration/7.1/* ../generated-pdf/release71/tutorials/en/product-integration/7.1/
 mkdir -p ../generated-pdf/release71/tutorials/en/quality-assurance/7.1 && cp -R _site/pdf/tutorials/en/quality-assurance/7.1/* ../generated-pdf/release71/tutorials/en/quality-assurance/7.1/
-cp -R _site/pdf/tutorials/en/7.1/application-scanning.pdf ../generated-pdf/release71/tutorials/en/application-scanning.pdf
+#cp -R _site/pdf/tutorials/en/7.1/application-scanning.pdf ../generated-pdf/release71/tutorials/en/application-scanning.pdf
 # copy the index file
 #cp _site/pdf/7.1.html ../generated-pdf/release71/index.html
 # commit and push generated content
