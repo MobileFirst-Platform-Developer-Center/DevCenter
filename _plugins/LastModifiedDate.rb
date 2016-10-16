fd1dcaf0974f1720b1bdac029da33b8d1b3c8ed8
Idan Adar <iadar@il.ibm.com>
2016-10-16 16:02:19 +0300

    Comment out changes done in #398
    Signed-off-by: Idan Adar <iadar@il.ibm.com>

--- _plugins/LastModifiedDate.rb
+++ _plugins/LastModifiedDate.rb
@@ -1,10 +1,10 @@
-module Jekyll
-  module MyFilters
-    def file_date(input)
-      File.mtime(input)
-    end
-  end
-end
-
-Liquid::Template.register_filter(Jekyll::MyFilters)
-
+# module Jekyll
+#   module MyFilters
+#     def file_date(input)
+#       File.mtime(input)
+#     end
+#   end
+# end
+# 
+# Liquid::Template.register_filter(Jekyll::MyFilters)
+# 
