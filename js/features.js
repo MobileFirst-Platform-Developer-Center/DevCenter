0154d463ff58e3992255d069ccc5fe1a742e6a56
Idan Adar <iadar@il.ibm.com>
2016-10-26 22:52:33 +0300

    Update menu
    Signed-off-by: Idan Adar <iadar@il.ibm.com>

--- js/features.js
+++ js/features.js
@@ -1,18 +1,28 @@
 // Features page navigation
 $(".feature-analytics").css("display", "block");
+$(".analytics").css("background","#c3e5f8");
 
 $(".feature-analytics-img").on("click", function() {
+    // Display the corresponding div element
     $(".feature-analytics").css("display","block");
     $(".feature-push").css("display","none");
     $(".feature-security").css("display","none");
     $(".feature-backend-logic").css("display","none");
     $(".feature-management").css("display","none");
     
+    // temp
     $(".analytics").attr("src","../assets/features/ios.svg");
     $(".push").attr("src","../assets/features/android.svg");
     $(".security").attr("src","../assets/features/android.svg");
     $(".backend-logic").attr("src","../assets/features/android.svg");
     $(".management").attr("src","../assets/features/android.svg");
+    
+    // Change background color for active feature
+    $(".analytics").css("background","#c3e5f8");
+    $(".push").css("background","white");
+    $(".security").css("background","white");
+    $(".backend-logic").css("background","white");
+    $(".management").css("background","white");
 });
 
 $(".feature-push-img").on("click", function() {
@@ -27,6 +37,12 @@ $(".feature-push-img").on("click", function() {
     $(".security").attr("src","../assets/features/android.svg");
     $(".backend-logic").attr("src","../assets/features/android.svg");
     $(".management").attr("src","../assets/features/android.svg");
+    
+    $(".analytics").css("background","white");
+    $(".push").css("background","#c3e5f8");
+    $(".security").css("background","white");
+    $(".backend-logic").css("background","white");
+    $(".management").css("background","white");
 });
 
 $(".feature-security-img").on("click", function() {
@@ -41,6 +57,12 @@ $(".feature-security-img").on("click", function() {
     $(".security").attr("src","../assets/features/ios.svg");
     $(".backend-logic").attr("src","../assets/features/android.svg");
     $(".management").attr("src","../assets/features/android.svg");
+    
+    $(".analytics").css("background","white");
+    $(".push").css("background","white");
+    $(".security").css("background","#c3e5f8");
+    $(".backend-logic").css("background","white");
+    $(".management").css("background","white");
 });
 
 $(".feature-backend-logic-img").on("click", function() {
@@ -55,6 +77,12 @@ $(".feature-backend-logic-img").on("click", function() {
     $(".security").attr("src","../assets/features/android.svg");
     $(".backend-logic").attr("src","../assets/features/ios.svg");
     $(".management").attr("src","../assets/features/android.svg");
+    
+    $(".analytics").css("background","white");
+    $(".push").css("background","white");
+    $(".security").css("background","white");
+    $(".backend-logic").css("background","#c3e5f8");
+    $(".management").css("background","white");
 });
 
 $(".feature-management-img").on("click", function() {
@@ -69,4 +97,10 @@ $(".feature-management-img").on("click", function() {
     $(".security").attr("src","../assets/features/android.svg");
     $(".backend-logic").attr("src","../assets/features/android.svg");
     $(".management").attr("src","../assets/features/ios.svg");
+    
+    $(".analytics").css("background","white");
+    $(".push").css("background","white");
+    $(".security").css("background","white");
+    $(".backend-logic").css("background","white");
+    $(".management").css("background","#c3e5f8");
 });
