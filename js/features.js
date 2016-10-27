150a7538740d65ff5df4f9f1a7312443a91a6739
Idan Adar <iadar@il.ibm.com>
2016-10-27 11:48:31 +0300

    New features page #519
    Signed-off-by: Idan Adar <iadar@il.ibm.com>

--- js/features.js
+++ js/features.js
@@ -1,10 +1,22 @@
 // Features page navigation
 
 // Default display on page load
-$(".feature-analytics").css("display", "block");
-$(".analytics").css("background","#c3e5f8");
+$(".feature-security").css("display", "block");
+$(".security").css("background","#c3e5f8");
 
 // Display different feature lists
+$(".feature-security-img").on("click", function() {
+    resetList();
+    $(".feature-security").css("display","block");
+    $(".security").css("background","#c3e5f8");
+});
+
+$(".feature-management-img").on("click", function() {
+    resetList();
+    $(".feature-management").css("display","block");
+    $(".management").css("background","#c3e5f8");
+});
+
 $(".feature-analytics-img").on("click", function() {
     resetList();
     $(".feature-analytics").css("display", "block");
@@ -23,29 +35,17 @@ $(".feature-push-img").on("click", function() {
     $(".push").css("background","#c3e5f8");
 });
 
-$(".feature-security-img").on("click", function() {
-    resetList();
-    $(".feature-security").css("display","block");
-    $(".security").css("background","#c3e5f8");
-});
-
-$(".feature-management-img").on("click", function() {
-    resetList();
-    $(".feature-management").css("display","block");
-    $(".management").css("background","#c3e5f8");
-});
-
 // Reset function
 function resetList() {
+    $(".feature-security").css("display","none");
+    $(".feature-management").css("display","none");
     $(".feature-analytics").css("display","none");
     $(".feature-backend-logic").css("display","none");
     $(".feature-push").css("display","none");
-    $(".feature-security").css("display","none");
-    $(".feature-management").css("display","none");
-    
+
+    $(".security").css("background","none");
+    $(".management").css("background","none");
     $(".analytics").css("background","none");
     $(".backend-logic").css("background","none");
     $(".push").css("background","none");
-    $(".security").css("background","none");
-    $(".management").css("background","none");
 }
