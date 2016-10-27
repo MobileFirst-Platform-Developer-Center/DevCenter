823e3ba1af6c3e987612ee24cde74e553496901e
Idan Adar <iadar@il.ibm.com>
2016-10-27 11:06:53 +0300

    Use a reset function to simplify code
    Signed-off-by: Idan Adar <iadar@il.ibm.com>

--- js/features.js
+++ js/features.js
@@ -1,75 +1,51 @@
 // Features page navigation
+
+// Default display on page load
 $(".feature-analytics").css("display", "block");
 $(".analytics").css("background","#c3e5f8");
 
+// Display different feature lists
 $(".feature-analytics-img").on("click", function() {
-    // Display the corresponding div element
-    $(".feature-analytics").css("display","block");
-    $(".feature-push").css("display","none");
-    $(".feature-security").css("display","none");
-    $(".feature-backend-logic").css("display","none");
-    $(".feature-management").css("display","none");
-    
-    // Change background color for active feature
+    resetList();
+    $(".feature-analytics").css("display", "block");
     $(".analytics").css("background","#c3e5f8");
-    $(".push").css("background","none");
-    $(".security").css("background","none");
-    $(".backend-logic").css("background","none");
-    $(".management").css("background","none");
-});
-
-$(".feature-push-img").on("click", function() {
-    $(".feature-analytics").css("display","none");
-    $(".feature-security").css("display","none");
-    $(".feature-backend-logic").css("display","none");
-    $(".feature-push").css("display","block");
-    $(".feature-management").css("display","none");
-    
-    $(".analytics").css("background","none");
-    $(".push").css("background","#c3e5f8");
-    $(".security").css("background","none");
-    $(".backend-logic").css("background","none");
-    $(".management").css("background","none");
-});
-
-$(".feature-security-img").on("click", function() {
-    $(".feature-push").css("display","none");
-    $(".feature-analytics").css("display","none");
-    $(".feature-backend-logic").css("display","none");
-    $(".feature-management").css("display","none");
-    $(".feature-security").css("display","block");
-    
-    $(".analytics").css("background","none");
-    $(".push").css("background","none");
-    $(".security").css("background","#c3e5f8");
-    $(".backend-logic").css("background","none");
-    $(".management").css("background","none");
 });
 
 $(".feature-backend-logic-img").on("click", function() {
-    $(".feature-push").css("display","none");
-    $(".feature-security").css("display","none");
-    $(".feature-analytics").css("display","none");
+    resetList();
     $(".feature-backend-logic").css("display","block");
-    $(".feature-management").css("display","none");
-    
-    $(".analytics").css("background","none");
-    $(".push").css("background","none");
-    $(".security").css("background","none");
     $(".backend-logic").css("background","#c3e5f8");
-    $(".management").css("background","none");
+});
+
+$(".feature-push-img").on("click", function() {
+    resetList();
+    $(".feature-push").css("display","block");
+    $(".push").css("background","#c3e5f8");
+});
+
+$(".feature-security-img").on("click", function() {
+    resetList();
+    $(".feature-security").css("display","block");
+    $(".security").css("background","#c3e5f8");
 });
 
 $(".feature-management-img").on("click", function() {
-    $(".feature-push").css("display","none");
-    $(".feature-security").css("display","none");
-    $(".feature-backend-logic").css("display","none");
-    $(".feature-analytics").css("display","none");
+    resetList();
     $(".feature-management").css("display","block");
-    
-    $(".analytics").css("background","none");
-    $(".push").css("background","none");
-    $(".security").css("background","none");
-    $(".backend-logic").css("background","none");
     $(".management").css("background","#c3e5f8");
 });
+
+// Reset function
+function resetList() {
+    $(".feature-analytics").css("display","none");
+    $(".feature-backend-logic").css("display","none");
+    $(".feature-push").css("display","none");
+    $(".feature-security").css("display","none");
+    $(".feature-management").css("display","none");
+    
+    $(".analytics").css("background","none");
+    $(".backend-logic").css("background","none");
+    $(".push").css("background","none");
+    $(".security").css("background","none");
+    $(".management").css("background","none");
+}
