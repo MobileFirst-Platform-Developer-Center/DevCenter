c0699ef17bd448a9b23778883d498d4f920b60e9
Idan Adar <iadar@il.ibm.com>
2016-11-01 15:13:00 +0200

    Formatting and removal of appscan tutorial
    appscan is now part of product integration
    
    Signed-off-by: Idan Adar <iadar@il.ibm.com>

--- build/buildPDF.sh
+++ build/buildPDF.sh
@@ -33,7 +33,7 @@ git clone git@github.ibm.com:MFPSamples/TutorialsForOfflineReading.git --depth 1
 rm -rf ../generated-pdf/release80/*
 mkdir -p ../generated-pdf/release80/tutorials/en/foundation/8.0 && cp -R _site/pdf/tutorials/en/foundation/8.0/* ../generated-pdf/release80/tutorials/en/foundation/8.0/
 mkdir -p ../generated-pdf/release80/tutorials/en/product-integration/8.0 && cp -R _site/pdf/tutorials/en/product-integration/8.0/* ../generated-pdf/release80/tutorials/en/product-integration/8.0/
-# mkdir -p ../generated-pdf/release80/tutorials/en/quality-assurance/8.0 && cp -R _site/pdf/tutorials/en/quality-assurance/8.0/* ../generated-pdf/release80/tutorials/en/quality-assurance/8.0/
+mkdir -p ../generated-pdf/release80/tutorials/en/quality-assurance/8.0 && cp -R _site/pdf/tutorials/en/quality-assurance/8.0/* ../generated-pdf/release80/tutorials/en/quality-assurance/8.0/
 
 # copy the index file
 cp _site/pdf/8.0.html ../generated-pdf/release80/index.html
@@ -52,14 +52,18 @@ cd ../../DevCenter
 pwd
 #clone pdf repository
 git clone git@github.ibm.com:MFPSamples/TutorialsForOfflineReading.git --depth 1 --branch release71 --single-branch ../generated-pdf/release71
+
 # copy generated PDF
 rm -rf ../generated-pdf/release71/*
+
 mkdir -p ../generated-pdf/release71/tutorials/en/foundation/7.1 && cp -R _site/pdf/tutorials/en/foundation/7.1/* ../generated-pdf/release71/tutorials/en/foundation/7.1/
+
 mkdir -p ../generated-pdf/release71/tutorials/en/product-integration/7.1 && cp -R _site/pdf/tutorials/en/product-integration/7.1/* ../generated-pdf/release71/tutorials/en/product-integration/7.1/
-# mkdir -p ../generated-pdf/release71/tutorials/en/quality-assurance/7.1 && cp -R _site/pdf/tutorials/en/quality-assurance/7.1/* ../generated-pdf/release71/tutorials/en/quality-assurance/7.1/
-#cp -R _site/pdf/tutorials/en/7.1/application-scanning.pdf ../generated-pdf/release71/tutorials/en/application-scanning.pdf
-# copy the index file
-#cp _site/pdf/7.1.html ../generated-pdf/release71/index.html
+
+mkdir -p ../generated-pdf/release71/tutorials/en/quality-assurance/7.1 && cp -R _site/pdf/tutorials/en/quality-assurance/7.1/* ../generated-pdf/release71/tutorials/en/quality-assurance/7.1/
+
+#copy the index file
+cp _site/pdf/7.1.html ../generated-pdf/release71/index.html
 
 # commit and push generated content
 cd ../generated-pdf/release71
@@ -75,13 +79,17 @@ cd ../../DevCenter
 pwd
 #clone pdf repository
 git clone git@github.ibm.com:MFPSamples/TutorialsForOfflineReading.git --depth 1 --branch release70 --single-branch ../generated-pdf/release70
+
 # copy generated PDF
 rm -rf ../generated-pdf/release70/*
+
 mkdir -p ../generated-pdf/release70/tutorials/en/foundation/7.0 && cp -R _site/pdf/tutorials/en/foundation/7.0/* ../generated-pdf/release70/tutorials/en/foundation/7.0/
+
 mkdir -p ../generated-pdf/release70/tutorials/en/product-integration/7.0 && cp -R _site/pdf/tutorials/en/product-integration/7.0/* ../generated-pdf/release70/tutorials/en/product-integration/7.0/
-#cp -R _site/pdf/tutorials/en/product-integration/7.0/application-scanning.pdf ../generated-pdf/release70/tutorials/en/application-scanning.pdf
+
 # copy the index file
 cp _site/pdf/7.0.html ../generated-pdf/release70/index.html
+
 # commit and push generated content
 cd ../generated-pdf/release70
 git config user.email "nathanh@il.ibm.com"
@@ -96,14 +104,17 @@ cd ../../DevCenter
 pwd
 #clone pdf repository
 git clone git@github.ibm.com:MFPSamples/TutorialsForOfflineReading.git --depth 1 --branch release63 --single-branch ../generated-pdf/release63
+
 # copy generated PDF
 rm -rf ../generated-pdf/release63/*
+
 mkdir -p ../generated-pdf/release63/tutorials/en/foundation/6.3 && cp -R _site/pdf/tutorials/en/foundation/6.3/* ../generated-pdf/release63/tutorials/en/foundation/6.3/
+
 mkdir -p ../generated-pdf/release63/tutorials/en/product-integration/6.3 && cp -R _site/pdf/tutorials/en/product-integration/6.3/* ../generated-pdf/release63/tutorials/en/product-integration/6.3/
-#cp -R _site/pdf/tutorials/en/production-integration/6.3/application-scanning.pdf ../generated-pdf/release63/tutorials/en/application-scanning.pdf
 
 # copy the index file
 cp _site/pdf/6.3.html ../generated-pdf/release63/index.html
+
 # commit and push generated content
 cd ../generated-pdf/release63
 git config user.email "nathanh@il.ibm.com"
