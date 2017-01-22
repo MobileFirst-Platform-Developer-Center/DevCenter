a9191d58d1582d408b4f0eb867f9118cc59a6200
Idan Adar <iadar@il.ibm.com>
2017-01-22 09:35:20 +0200

    Delete unused plug-ins
    Signed-off-by: Idan Adar <iadar@il.ibm.com>

--- _plugins/Redcarpet_Patch.rb
+++
@@ -1,9 +0,0 @@
-# require 'redcarpet'
-# class Redcarpet::Render::HTML
-#   # A patch to allow Japanese headers.
-#   # Remove once this is fixed and released:
-#   # https://github.com/vmg/redcarpet/issues/538
-#   def header(text, header_level)
-#     %Q{<h#{header_level} id="#{text.downcase.gsub(" ", "-")}">#{text}</h#{header_level}>}
-#   end
-# end
